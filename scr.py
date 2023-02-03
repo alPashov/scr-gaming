b2f5d9b58eacec938ae87f0f169e4e454d647ff9
pr0UnDead <pashovaz@abv.bg>
2023-02-03 08:38:50 +0200

    Exporting to csv

--- scr.py
+++ scr.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup as bs
+import csv
 
 url = 'https://www.emag.bg/laptopi/filter/kapacitet-pamet-f7886,12-16-gb-v25916/kapacitet-pamet-f7886,nad-16-gb-v25917/diagonal-displej-f7887,15-15-6-v25847/tip-video-pamet-f7895,dopylnitelna-v-2754/tip-syhranenie-f7941,ssd-v-4670679/video-chipset-f8356,nvidia-geforce-rtx-v-7220419/kapacitet-ssd-f9203,500-gb-v-4694038/kapacitet-ssd-f9203,1-tb-v-4690219/kapacitet-ssd-f9203,512-gb-v-4685751/price,between-1499-and-3000/c'
 
@@ -26,4 +27,8 @@ links = []
 for href in soup.select(".card-v2-info a"):
     links.append(href["href"])
 
-print(links)
+with open("links.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Link"])
+    for href in links:
+        writer.writerow([href])
