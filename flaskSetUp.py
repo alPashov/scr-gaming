71afe867d875ff91db8ebc871e5c0ff28d0c5fa8
pr0UnDead <pashovaz@abv.bg>
2023-02-04 22:24:07 +0200

    *added automated opening for localhost

--- flaskSetUp.py
+++ flaskSetUp.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
-from myScraper import links as links, soup as soup
+import webbrowser
+from allPagesScr import main as scr
 
-
+links = scr()
 
 app = Flask(__name__)
 
 @app.route("/")
 def gaming():
-    return render_template('gaming.html', links = links, soup = soup)
+    return render_template('gaming.html', links = links)
 
-app.run(host="0.0.0.0", port=5050)
+if __name__ == "__main__":
+    webbrowser.open_new_tab("localhost:5050")
+    app.run(host="0.0.0.0", port=5050)
