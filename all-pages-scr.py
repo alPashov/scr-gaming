63b75976dfb7e87b24eeda1a1c9a3c419ce6ab22
pr0UnDead <pashovaz@abv.bg>
2023-02-04 09:05:07 +0200

    splitted links list for each link (category)

--- all-pages-scr.py
+++ all-pages-scr.py
@@ -1,14 +1,13 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup as bs
 import csv
-import pandas as pd
 
 urls = [
-    #Gaming Links 
+    #Gaming Links 40
     "https://www.emag.bg/laptopi/filter/kapacitet-pamet-f7886,12-16-gb-v25916/kapacitet-pamet-f7886,nad-16-gb-v25917/tip-video-pamet-f7895,dopylnitelna-v-2754/tip-syhranenie-f7941,ssd-v-4670679/price,between-1000-and-3000/rating,star-4/c?ref=lst_leftbar_6420_4-5",
-    #Everyday/Home
+    #Everyday/Home 29
     "https://www.emag.bg/laptopi/filter/kapacitet-pamet-f7886,6-8-gb-v25915/tip-video-pamet-f7895,vgradena-v-2755/price,between-500-and-1000/rating,star-4/c?ref=lst_leftbar_6420_4-5",
-    #Office
+    #Office 31
     "https://www.emag.bg/laptopi/filter/kapacitet-pamet-f7886,12-16-gb-v25916/tip-video-pamet-f7895,vgradena-v-2755/avtonomija-baterija-f7901,nad-6-h-v25862/tip-syhranenie-f7941,ssd-v-4670679/kapacitet-ssd-f9203,500-gb-v-4694038/kapacitet-ssd-f9203,1-tb-v-4690219/kapacitet-ssd-f9203,512-gb-v-4685751/price,between-979-and-5732/rating,star-4/c"
 
 ]
@@ -29,14 +28,14 @@ def html_parsers(htmls):
     for h in htmls:
         soups = bs(h, "html.parser")
         items = soups.select("div.card-v2-wrapper")
-
+        items_page = []
         for i in items:
             href = i.select_one("div.card-v2-info a").attrs["href"]
             heading2 = i.select_one("div.pad-hrz-xs h2.card-v2-title-wrapper a").text.strip()
             price = i.select_one("div.card-v2-content div.card-v2-pricing p.product-new-price").text.strip()
             product = (heading2, price, href)
-            links.append(product)
-    
+            items_page.append(product)
+        links.append(items_page)
         # links.append(href)
     return links
 
