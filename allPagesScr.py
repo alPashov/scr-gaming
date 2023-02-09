8d22d276134c57753f651a926ca6d60e842f5b87
pr0UnDead <pashovaz@abv.bg>
2023-02-09 17:40:54 +0200

    *added avaibility check

--- allPagesScr.py
+++ allPagesScr.py
@@ -36,7 +36,8 @@ def html_parsers(htmls):
             heading2 = i.select_one("div.pad-hrz-xs h2.card-v2-title-wrapper a").text.strip()
             price = i.select_one("div.card-v2-content div.card-v2-pricing p.product-new-price").text.strip()
             img_src = i.select_one("div.card-v2-info div.card-v2-thumb-inner img.w-100").attrs['src']
-            product = (heading2, price, href, img_src)
+            avaibility = i.select_one("div.card-v2-wrapper div.card-v2-info div.pad-hrz-xs div.card-estimate-placeholder div")
+            product = (heading2, price, href, img_src, avaibility)
             items_page.append(product)
         links.append(items_page)
         # links.append(href)
