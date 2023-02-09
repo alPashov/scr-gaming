88b29ed3fbe55a1fb9c51f777fe07e6c16bc1eeb
pr0UnDead <pashovaz@abv.bg>
2023-02-09 09:16:39 +0200

    *comments edit

--- allPagesScr.py
+++ allPagesScr.py
@@ -5,11 +5,11 @@ from array import *
 import pandas as pd
 
 urls = [
-    #Gaming Links 40
+    #Gaming Links 
     "https://www.emag.bg/laptopi/filter/kapacitet-pamet-f7886,12-16-gb-v25916/kapacitet-pamet-f7886,nad-16-gb-v25917/tip-video-pamet-f7895,dopylnitelna-v-2754/tip-syhranenie-f7941,ssd-v-4670679/price,between-1000-and-3000/rating,star-4/c?ref=lst_leftbar_6420_4-5",
-    #Everyday/Home 29
+    #Everyday/Home 
     "https://www.emag.bg/laptopi/filter/kapacitet-pamet-f7886,6-8-gb-v25915/tip-video-pamet-f7895,vgradena-v-2755/price,between-500-and-1000/rating,star-4/c?ref=lst_leftbar_6420_4-5",
-    #Office 31
+    #Office 
     "https://www.emag.bg/laptopi/filter/kapacitet-pamet-f7886,12-16-gb-v25916/tip-video-pamet-f7895,vgradena-v-2755/avtonomija-baterija-f7901,nad-6-h-v25862/tip-syhranenie-f7941,ssd-v-4670679/kapacitet-ssd-f9203,500-gb-v-4694038/kapacitet-ssd-f9203,1-tb-v-4690219/kapacitet-ssd-f9203,512-gb-v-4685751/price,between-979-and-5732/rating,star-4/c"
 
 ]
@@ -62,7 +62,7 @@ def print2Darr(links):
 def main():
     htmls = get_html()
     links = html_parsers(htmls)
-    # csv_export(links) 
+    csv_export(links) 
     Dframe = pd.DataFrame(links)
     print(Dframe)
     # print2Darr(links)
